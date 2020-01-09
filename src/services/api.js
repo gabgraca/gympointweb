c8986b216f00deb00bd0903bc8bd6b3bd745d65d
Gabriel <gabriel.abner@gmail.com>
2020-01-09 15:56:19 -0300

    teste de deploy no netlify

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://gympoint.api.gabdev.com.br:3333',
+  baseURL: 'http://gympoint.api.gabdev.com.br:3334',
 });
 
 export default api;
